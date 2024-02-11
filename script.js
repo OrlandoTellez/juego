c03fd5df79026771f929485dfad540c5ba3136f3
Orlando <espadachinkinno@gmail.com>
2024-02-11 12:19:48 -0600

    add: commit commit

--- script.js
+++ script.js
@@ -1,37 +1,15 @@
-// let rockHealth = 100;
-// let characterPosition = 0;
-
-// // Agregar evento de teclado para mover al personaje
-// document.addEventListener('keydown', (event) => {
-//     const key = event.key;
-//     if (key === 'ArrowRight') {
-//         characterPosition += 10; // Mover hacia la derecha
-//     } else if (key === 'ArrowLeft') {
-//         characterPosition -= 10; // Mover hacia la izquierda
-//     }
-//     document.querySelector('.character').style.left = characterPosition + 'px';
-// });
-
-// // Agregar evento de clic para golpear la roca
-// document.querySelector('.character').addEventListener('click', () => {
-//     if (rockHealth > 0) {
-//         rockHealth -= 10; // Reducir la vida de la roca al golpearla
-//         console.log('Vida de la roca:', rockHealth);
-//         if (rockHealth <= 0) {
-//             document.querySelector('.rock').style.display = 'none'; // Desaparecer la roca
-//             console.log('¡La roca ha desaparecido!');
-//         }
-//     }
-// });
-
 let canvas = document.getElementById("myCanvas");
 let ctx = canvas.getContext("2d");
-var ballRadius = 30;
-var x = canvas.width/2;
-var y = canvas.height-30;
-var dx = 2;
-var dy = -2;
-
+let ballRadius = 30;
+let x = canvas.width/2;
+let y = canvas.height-30;
+let dx = 2;
+let dy = -2;
+let paddleHeight = 10;
+let paddleWidth = 75;
+let paddleX = (canvas.width - paddleWidth) / 2; 
+let rightPressed = false;
+let leftPressed = false;
 function drawBall(){
     ctx.beginPath();
     ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
@@ -39,21 +17,53 @@ function drawBall(){
     ctx.fill();
     ctx.closePath();
 }
-
+function drawPaddle(){
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
 function draw(){
     ctx.clearRect(0, 0, canvas.width, canvas.height)
+    drawPaddle();
     drawBall();
+
     if (x + dx > canvas.width - ballRadius|| x + dx < ballRadius) {
-        dx = -dx;
-        
-      }
-    if(y + dy > canvas.height - ballRadius || y + dy < ballRadius){
+        dx = -dx;    
+    }
+    if (y + dy < ballRadius) {
         dy = -dy;
+    }else if (y + dy > canvas.height - ballRadius) {
+        if (x > paddleX && x < paddleX + paddleWidth) {
+            dy = -dy;
+          } else {
+            alert("GAME OVER");
+            document.location.reload();
+          }
     }
     x += dx;
     y += dy;
-    
-    
+    if (rightPressed && paddleX < canvas.width - paddleWidth) {
+        paddleX += 7;
+    }else if (leftPressed && paddleX > 0) {
+        paddleX -= 7;
+    }
 }
-
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false)
+function keyDownHandler(e){
+    if(e.keyCode == 39){
+        rightPressed = true;
+    } else if(e.keyCode == 37){
+        leftPressed = true;
+    }
+}
+function keyUpHandler(e){
+    if(e.keyCode == 39){
+        rightPressed = false;
+    } else if(e.keyCode == 37){
+        leftPressed = false;
+    }
+}
 setInterval(draw, 10);
